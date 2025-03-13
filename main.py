d4b4de561abe6a42cfe08fba3a683439d8f1aba8
Ricky Klasson <mrklasson@gmail.com>
2025-03-13 10:46:29 +0100

    First fully functional prototype

--- main.py
+++ main.py
@@ -25,11 +25,6 @@ class VideoFile:
     def overlaps(self, other: 'VideoFile') -> tuple['VideoFile', 'VideoFile'] | None:
         if self.start_time > other.end_time or self.end_time < other.start_time:
             return None
-
-        overlap_start = max(self.start_time, other.start_time)
-        overlap_end = min(self.end_time, other.end_time)
-        duration = (overlap_end - overlap_start).total_seconds()
-
         return (self, other)
 
 
@@ -75,6 +70,8 @@ def get_video_files_from_folder(folder: Path) -> list[VideoFile]:
         if entry.is_file() and entry.suffix in ['.mp4', '.mkv']:
             video_files.append(path_to_video_file(entry))
 
+    print(f'---- Found {len(video_files)} recordings in folder: {folder}')
+
     return video_files
 
 
@@ -86,6 +83,8 @@ def get_overlapping_pairs(video_files_0: list[VideoFile],
             overlap = file_0.overlaps(file_1)
             if overlap is not None:
                 overlaps.append(overlap)
+
+    print(f'---- Found {len(overlaps)} overlapping recordings')
 
     return overlaps
 
@@ -114,12 +113,11 @@ def extract_audio(video_path: Path, audio_path: Path):
            '-hide_banner',
            '-stats',
            '-y', str(audio_path)]
-    print(f'Found {num_audio_streams} audio stream(s). Extracting to file via command: \n{" ".join(cmd)}')
+    print(f'-- Merging {num_audio_streams} audio stream for synchronization...')
     subprocess.run(cmd, text=True, check=True)
 
 
 def load_audio(audio_path: Path) -> tuple:
-    print(f'Loading audio from: {audio_path}')
     sr, audio = scipy.io.wavfile.read(audio_path)
     return sr, audio
 
@@ -139,6 +137,8 @@ def find_audio_offset(sample_rate, audio0, audio1) -> float:
 
 def sync_videos_by_audio(vid0: VideoFile, vid1: VideoFile):
     """Modifies overlap in place when syncing the video clips using audio correlation."""
+    print('-- Syncing files by audio...')
+
     TMP_FOLDER = Path('./tmp')
     TMP_FOLDER.mkdir(exist_ok=True, parents=False)
 
@@ -154,7 +154,7 @@ def sync_videos_by_audio(vid0: VideoFile, vid1: VideoFile):
         raise ValueError('Audio sample rates do not match')
 
     offset = find_audio_offset(sr0, audio0, audio1)
-    print(f'Offset between files: {offset:.3f}s')
+    print(f'-- Audio offset between files: {offset:.3f}s')
 
     # Adjust overlap from audio offset.
     if offset > 0:
@@ -162,6 +162,7 @@ def sync_videos_by_audio(vid0: VideoFile, vid1: VideoFile):
         duration_after_trim = vid0.duration - offset
         duration_out = min(duration_after_trim, vid1.duration)
     else:
+        offset = abs(offset)
         vid1.trim_start = offset
         duration_after_trim = vid1.duration - offset
         duration_out = min(duration_after_trim, vid0.duration)
@@ -174,12 +175,17 @@ def sync_videos_by_audio(vid0: VideoFile, vid1: VideoFile):
 
 def reencode_overlapping(vid0: VideoFile, vid1: VideoFile, out_folder: Path):
     """Re-encode videos into a split-screen view."""
+    print(f'---- Re-encoding overlapping files:\n\t\t{vid0.path}\n\t\t{vid1.path}')
+
     out_folder.mkdir(exist_ok=True, parents=True)
 
     sync_videos_by_audio(vid0, vid1)
 
     timestamp = (vid0.start_time + timedelta(seconds=vid0.trim_start)).strftime('%Y-%m-%d__%H-%M-%S')
     out_name = f'SplitView__{timestamp}.mkv'
+    out_path = out_folder / out_name
+
+    print(f'-- Re-encoding split screen view of recordings to file: {out_path}')
     cmd = ['ffmpeg',
            '-i', str(vid0.path), '-i', str(vid1.path),
            '-filter_complex',
@@ -187,26 +193,29 @@ def reencode_overlapping(vid0: VideoFile, vid1: VideoFile, out_folder: Path):
            'setpts=PTS-STARTPTS[v0];'
            f'[1:v]scale=1280:720,crop=640:720:320:0,trim=start={vid1.trim_start}:end={vid1.trim_end},'
            'setpts=PTS-STARTPTS[v1];'
-           '[v0][v1]hstack=inputs=2[vout]',
+           '[v0][v1]hstack=inputs=2[vout];'
+           f'[1:a]atrim=start={vid1.trim_start}:end={vid1.trim_end},asetpts=PTS-STARTPTS[aout];',
            '-map', '[vout]',
+           '-map', '[aout]',
            '-c:v', 'libx264',
            '-strict', 'experimental',
            '-vsync', 'vfr',
            '-loglevel', 'quiet',
            '-hide_banner',
            '-stats',
-           '-y', str(out_folder / out_name)]
+           '-y', str(out_path)]
 
     subprocess.run(cmd, text=True, check=True)
 
 
 def main(args):
+    print('-------- SPLIT SCREEN ENCODER --------')
     video_files_0 = get_video_files_from_folder(args.folders[0])
     video_files_1 = get_video_files_from_folder(args.folders[1])
 
     overlaps = get_overlapping_pairs(video_files_0, video_files_1)
 
-    [reencode_overlapping(*o, args.out) for o in overlaps[-1:]]
+    [reencode_overlapping(*o, args.out) for o in overlaps]
 
 
 if __name__ == '__main__':
